fa25c323babf21eae475e3c0172e6a2d4ccc0cbc
Kyle Tenzeldam and Ryan Ferris <ryan@>
2017-11-30 08:31:51 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you????"));
+
+  if (age < 18) {
+    $("#CANDIDATES").show();
+  }
+});
+//
+// $(document).ready(function() {
+//   var age = parseInt(prompt("How old are you?"));
+//
+//   if (age < 18) {
+//     $("#CANDIDATES").show();
+//   }
+// })
