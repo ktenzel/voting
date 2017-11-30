2820915e6a49fe507ec07c918a416d9b60ebbffc
Kyle Tenzeldam and Ryan Ferris <kyle@>
2017-11-30 09:09:56 -0800

    update js/scripts.js and css/styles.css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you????"));
 
-  if (age < 18) {
+  if (age >= 18) {
     $("#CANDIDATES").show();
+  } else {
+    $("#under-18").show();
   }
 });
 //
